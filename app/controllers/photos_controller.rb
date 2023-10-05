8aa301fb8bc5389c3972f8c4e754845d64e3ba05
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-05 21:14:22 +0000

    created photos index action

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   def index
+    @list_of_photos = Photo.all.order( :created_at => :desc)
     render({ :template => "photos_templates/index"})
   end
 end
