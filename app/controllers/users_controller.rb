a61dcc91c9263eb4fc0fc8cd8d21455570b4c4ed
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-05 19:31:05 +0000

    sorting usernames in alphabetical order

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @list_of_users = User.all.order( :created_at => :desc )
+    @list_of_users = User.all.order( :username => :asc )
 
     render({ :template => "user_templates/index" })
   end
