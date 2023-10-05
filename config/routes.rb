8c7dc9b5932ab695394801048dd246703ee36127
Alexandra Mompremier <58926246+ajmompr@users.noreply.github.com>
2023-10-05 19:38:29 +0000

    updated users details page route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get("/", { :controller => "users", :action => "index"})
 
   get("/users", { :controller => "users", :action => "index"})
-  get("/users/:username", { :controller => "users", :action => "show"} )
+  get("/users/:path_username", { :controller => "users", :action => "show"} )
 end
